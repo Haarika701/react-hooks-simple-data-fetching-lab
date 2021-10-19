842e080314a38993809b75390137aa7b41061b90
HaarikaR <hramadugu@gmail.com>
2021-10-19 14:59:40 -0700

    commit tree 5228c2bae546e60eeec012ed02cd23300dc019a3

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React,{useEffect ,useState} from "react";
+
+function App() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random/3")
+      .then((r) => r.json())
+      .then((data) => {
+        // setting state in the useEffect callback
+        setImages(data.message);
+      },[]);
+  });
+
+  return (
+    <div>
+      {/* {images.map((image) => (
+        <img src={image} key={image} alt = "A Random Dog" />
+      ))} */}
+      <p>Loading....</p>
+    </div>
+  );
+}
+export default App;
